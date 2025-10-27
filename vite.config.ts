4169f837bb1febcbbd38114a1b279b65885ab295
mateuslimaluz-blip <mateus.lima.luz@escola.pr.gov.br>
2025-10-27 10:19:24 -0300

    Vite config.ts pro renan

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/RPG_RENAN/',  // <--- ADICIONE ESTA LINHA AQUI
   server: {
     host: "::",
     port: 8080,
